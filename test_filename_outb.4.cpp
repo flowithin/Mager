e00ac1a62da312cf1f3759b43fb73afcc7d5726b
jtwuuuuu <1874216549@qq.com>
2025-03-25 23:55:24 -0400

    test whole

--- test_filename_outb.4.cpp
+++ test_filename_outb.4.cpp
@@ -10,7 +10,17 @@ using std::cout;
 int main() {
   /* Allocate swap-backed page from the arena */
   pid_t pid = fork();
-  if(pid != 0){
+  pid_t pid2 = fork();
+if(pid2 == 0){
+    char* filename33 = static_cast<char *>(vm_map(nullptr, 0));
+    char* filename34 = static_cast<char *>(vm_map(nullptr, 0));
+    strcpy(filename33+VM_PAGESIZE-4, "papers.txt");
+    strcpy(filename34+VM_PAGESIZE-9, "papers.txt");
+  char* pp = static_cast<char *>(vm_map (filename33+VM_PAGESIZE-4, 0));
+    if(pp != nullptr) pp[0]++;
+  char* p11 = static_cast<char *>(vm_map (filename34+VM_PAGESIZE-9, 1));
+    std::cout << p11[0];
+  } else if(pid != 0){
   char* filename = static_cast<char *>(vm_map(nullptr, 0));
   /* Write the name of the file that will be mapped */
   //all pointing to 0
@@ -22,12 +32,10 @@ int main() {
   char* p = static_cast<char *>(vm_map (filename+VM_PAGESIZE, 0));
 
   /* Print the first part of the paper */
-  for (unsigned int i=0; i<10; i++) {
     //need evicting(bring from file)
-    cout << p[i];
+    if(p!=nullptr)
+    cout << p[0];
     //write fault
-    p[i] ++;
-  }
   } else {
     std::cout << "child process running:\n";
   /* Allocate swap-backed page from the arena */
@@ -70,7 +78,7 @@ int main() {
   for (unsigned int i=0; i<10; i++) {
     cout << p[i];
   }
+  
   }
-
 
 }
